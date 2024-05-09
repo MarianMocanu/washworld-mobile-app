43880a800fe5602b666d467474db74c32aa51794
Marian <mocanumarian1993@gmail.com>
2024-05-09 11:57:00 +0200

    added logo and styled bottom tabs

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -4,6 +4,9 @@ import { MaterialIcons } from '@expo/vector-icons';
 import { DashboardScreen } from 'src/features/dashboard/DashboardScreen';
 import { StartWashScreen } from 'src/features/startwash/StartWashScreen';
 import { AccountScreen } from 'src/features/account/AccountScreen';
+import { LogoSVG } from 'src/assets/SVGIcons';
+import { Text, View } from 'react-native';
+import { colors } from '@globals/globalStyles';
 
 export type TabsParamList = {
   dashboard: undefined;
@@ -13,26 +16,88 @@ export type TabsParamList = {
 
 const Tab = createBottomTabNavigator<TabsParamList>();
 
+type TabBarIconProps = {
+  focused: boolean;
+  color: string;
+  size: number;
+};
+
 export default function TabNavigator() {
+  function getTabBarLabelStyle(focused: boolean, color: string) {
+    return {
+      color: focused ? colors.primary.base : colors.grey[60],
+      fontFamily: 'gilroy-semibold',
+      fontSize: 12,
+      lineHeight: 12,
+    };
+  }
+
   return (
     <Tab.Navigator
       screenOptions={({ route }) => ({
-        headerShown: false,
         tabBarIcon: ({ focused, color, size }) => {
           switch (route.name) {
             case 'dashboard':
-              return <MaterialIcons name="dashboard" size={24} color={color} />;
+              return (
+                <MaterialIcons
+                  name="dashboard"
+                  size={24}
+                  color={focused ? colors.primary.base : colors.grey[60]}
+                />
+              );
             case 'start-wash':
-              return <MaterialIcons name="local-car-wash" size={24} color={color} />;
+              return (
+                <MaterialIcons
+                  name="local-car-wash"
+                  size={24}
+                  color={focused ? colors.primary.base : colors.grey[60]}
+                />
+              );
             case 'account':
-              return <MaterialIcons name="person" size={24} color={color} />;
+              return (
+                <MaterialIcons
+                  name="person"
+                  size={24}
+                  color={focused ? colors.primary.base : colors.grey[60]}
+                />
+              );
           }
         },
+
+        header: () => (
+          <View style={{ flex: 1, alignItems: 'center' }}>
+            <LogoSVG />
+          </View>
+        ),
       })}
     >
-      <Tab.Screen name="dashboard" component={DashboardScreen} />
-      <Tab.Screen name="start-wash" component={StartWashScreen} />
-      <Tab.Screen name="account" component={AccountScreen} />
+      <Tab.Screen
+        name="dashboard"
+        component={DashboardScreen}
+        options={{
+          tabBarLabel: ({ focused, color }) => (
+            <Text style={getTabBarLabelStyle(focused, color)}>Dashboard</Text>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="start-wash"
+        component={StartWashScreen}
+        options={{
+          tabBarLabel: ({ focused, color }) => (
+            <Text style={getTabBarLabelStyle(focused, color)}>Start Wash</Text>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="account"
+        component={AccountScreen}
+        options={{
+          tabBarLabel: ({ focused, color }) => (
+            <Text style={getTabBarLabelStyle(focused, color)}>Account</Text>
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
