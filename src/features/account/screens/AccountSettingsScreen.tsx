6d179a08fcf647cb6b663ed4073cb0cee5660d92
ThordurTh <thordur921@gmail.com>
2024-05-10 18:19:08 +0200

    Reusable button comp

--- src/features/account/screens/AccountSettingsScreen.tsx
+++ src/features/account/screens/AccountSettingsScreen.tsx
@@ -1,14 +1,4 @@
-import {
-  View,
-  Text,
-  StyleSheet,
-  ScrollView,
-  Button,
-  Switch,
-  Pressable,
-  TouchableOpacity,
-  Image,
-} from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Switch, Pressable } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { colors, globalTextStyles } from '@globals/globalStyles';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
@@ -16,6 +6,22 @@ import { AccStackParamList } from '../AccountScreen';
 import { RewardsIcon } from '@shared/RewardsIcon';
 
 type Props = NativeStackScreenProps<AccStackParamList, 'AccountSettings'>;
+
+interface ButtonProps {
+  text: string;
+  onPress: () => void;
+}
+
+const SettingsButton: React.FC<ButtonProps> = ({ text, onPress }) => {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={({ pressed }) => [{ backgroundColor: pressed ? '#FDFDFD' : 'transparent' }, styles.button]}
+    >
+      {({ pressed }) => <Text style={{ opacity: pressed ? 0.2 : 1 }}>{text}</Text>}
+    </Pressable>
+  );
+};
 
 const AccountSettingsScreen = (props: Props) => {
   return (
@@ -50,41 +56,31 @@ const AccountSettingsScreen = (props: Props) => {
         <View style={styles.section}>
           <Text style={styles.sectionHeading}>Subscription</Text>
           <View style={styles.buttonContainer}>
-            <TouchableOpacity
-              style={styles.button}
+            <SettingsButton
+              text="Change subscription"
               onPress={() => props.navigation.navigate('ChangeSubscription')}
-            >
-              <Text style={styles.buttonText}>Change Subscription</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.button}
+            />
+            <SettingsButton
+              text="Loyalty rewards"
               onPress={() => props.navigation.navigate('LoyaltyRewards')}
-            >
-              <Text>Loyalty rewards</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.button}
+            />
+            <SettingsButton
+              text="Add another car"
               onPress={() => props.navigation.navigate('AddAnotherCar')}
-            >
-              <Text>Add another car</Text>
-            </TouchableOpacity>
+            />
           </View>
 
           <View style={styles.section}>
             <Text style={styles.sectionHeading}>Preferences</Text>
             <View style={styles.buttonContainer}>
-              <TouchableOpacity
-                style={styles.button}
+              <SettingsButton
+                text="Preferred location"
                 onPress={() => props.navigation.navigate('PreferredLocation')}
-              >
-                <Text style={styles.buttonText}>Preferred location</Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={styles.button}
+              />
+              <SettingsButton
+                text="Preferred services"
                 onPress={() => props.navigation.navigate('PreferredServices')}
-              >
-                <Text style={styles.buttonText}>Preferred services</Text>
-              </TouchableOpacity>
+              />
               <View style={styles.button}>
                 <Text>Notifications</Text>
                 <Switch value={true} onValueChange={value => console.log('Notifications', value)} />
@@ -100,42 +96,30 @@ const AccountSettingsScreen = (props: Props) => {
           <View style={styles.section}>
             <Text style={styles.sectionHeading}>Help & Support</Text>
             <View style={styles.buttonContainer}>
-              <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate('FAQ')}>
-                <Text style={styles.buttonText}>FAQs</Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={styles.button}
+              <SettingsButton text="FAQs" onPress={() => props.navigation.navigate('FAQ')} />
+              <SettingsButton
+                text="Customer support"
                 onPress={() => props.navigation.navigate('CustomerSupport')}
-              >
-                <Text style={styles.buttonText}>Customer support</Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={styles.button}
+              />
+              <SettingsButton
+                text="Submit feedback"
                 onPress={() => props.navigation.navigate('SubmitFeedback')}
-              >
-                <Text style={styles.buttonText}>Submit feedback</Text>
-              </TouchableOpacity>
+              />
             </View>
           </View>
 
           <View style={[styles.section, styles.lastSection]}>
             <Text style={styles.sectionHeading}>Account</Text>
             <View style={styles.buttonContainer}>
-              <TouchableOpacity
-                style={styles.button}
+              <SettingsButton
+                text="Edit account details"
                 onPress={() => props.navigation.navigate('EditAccountDetails')}
-              >
-                <Text style={styles.buttonText}>Edit account details</Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={styles.button}
+              />
+              <SettingsButton
+                text="Change password"
                 onPress={() => props.navigation.navigate('ChangePassword')}
-              >
-                <Text style={styles.buttonText}>Change password</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate('LogOut')}>
-                <Text style={styles.buttonText}>Log out</Text>
-              </TouchableOpacity>
+              />
+              <SettingsButton text="Log out" onPress={() => props.navigation.navigate('LogOut')} />
             </View>
           </View>
         </View>
@@ -165,6 +149,7 @@ const styles = StyleSheet.create({
     gap: 12,
     paddingVertical: 12,
     marginTop: 12,
+    borderRadius: 8,
   },
   userIcon: {
     backgroundColor: '#34B566',
@@ -195,14 +180,16 @@ const styles = StyleSheet.create({
     paddingBottom: 16,
     backgroundColor: colors.white.base,
   },
-  buttonContainer: {},
+  buttonContainer: {
+    backgroundColor: colors.white.cream,
+    borderRadius: 4,
+  },
   button: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     paddingHorizontal: 12,
     paddingVertical: 12,
-    backgroundColor: colors.white.cream,
   },
   buttonText: {
     color: colors.black.base,
