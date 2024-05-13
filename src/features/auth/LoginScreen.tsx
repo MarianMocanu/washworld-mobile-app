5c32c3dae1bb2fba73c1345cbfd3b09e36bc2684
Fryderyk Boncler <f.relcnob@gmail.com>
2024-05-13 20:24:56 +0200

    feat: added Input component

--- src/features/auth/LoginScreen.tsx
+++ src/features/auth/LoginScreen.tsx
@@ -1,16 +1,27 @@
 import { NavigationProp, useNavigation } from '@react-navigation/native';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import Input from 'src/components/ui/Input';
 import { AuthStackParamList } from 'src/navigation/AuthNavigator';
 
 type Props = {};
 
 export const LoginScreen: FC<Props> = () => {
   const navigation = useNavigation<NavigationProp<AuthStackParamList, 'login'>>();
+  const [email, setEmail] = useState('');
 
   return (
     <View style={styles.container}>
       <Text>login</Text>
+      {/* just an example for now */}
+      <Input
+        placeholder="Email"
+        isValid={email && email.length < 3 ? true : false}
+        errorMessage="Email is required"
+        onChangeText={text => {
+          setEmail(text);
+        }}
+      ></Input>
       <Button title="Login" onPress={() => navigation.navigate('signup')} />
     </View>
   );
