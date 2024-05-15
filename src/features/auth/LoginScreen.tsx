43f5cab9024b86e6e2803a40a4f552b112c23c8b
Marian <mocanumarian1993@gmail.com>
2024-05-15 13:27:34 +0200

    moved input

--- src/features/auth/LoginScreen.tsx
+++ src/features/auth/LoginScreen.tsx
@@ -1,7 +1,7 @@
 import { NavigationProp, useNavigation } from '@react-navigation/native';
+import Input from '@shared/Input';
 import { FC, useState } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
-import Input from 'src/components/ui/Input';
 import { AuthStackParamList } from 'src/navigation/AuthNavigator';
 
 type Props = {};
