0cd2f5efc3112173ffd509f199c990fd685aa709
Marian <mocanumarian1993@gmail.com>
2024-05-20 17:42:09 +0200

    hotfix

--- App.tsx
+++ App.tsx
@@ -9,7 +9,6 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 export default function App() {
-  const queryClient = new QueryClient();
   const [appReady, setAppReady] = useState(false);
   const [location, setLocation] = useState(null);
   const [errorMessage, setErrorMessage] = useState<string>();
