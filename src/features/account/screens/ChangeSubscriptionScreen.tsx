ce0f21494f916788317a8ebe190491b88e42ca77
ThordurTh <thordur921@gmail.com>
2024-05-21 14:51:31 +0200

    comp of list of subscr. plans + levels query

--- src/features/account/screens/ChangeSubscriptionScreen.tsx
+++ src/features/account/screens/ChangeSubscriptionScreen.tsx
@@ -2,16 +2,17 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 import { AccountStackParamList } from '../AccountNavigator';
+import { SubscriptionsList } from 'src/features/stacks/subscripton/add/components/SubscriptionsList';
 
 type Props = NativeStackScreenProps<AccountStackParamList, 'subscription'>;
 
 const ChangeSubscriptionScreen = (props: Props) => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Change Subscription Screen</Text>
-      <Button title="Go back" onPress={() => props.navigation.goBack()} />
+      <SubscriptionsList />
     </View>
   );
 };
 
 export default ChangeSubscriptionScreen;
+
---
+++ src/features/stacks/subscripton/add/components/SubscriptionsList.tsx
@@ -0,0 +1,34 @@
+import { Text, View } from 'react-native';
+import RadioButton from './RadioButton';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+import { fetchLevels, useLevels } from '@queries/Levels';
+
+export const SubscriptionsList = () => {
+  const [selectedValue, setSelectedValue] = useState<number | null>(null);
+  // const disabledOption = useSelector((state: RootState) => something.users.current.subscription
+
+  const { data, isLoading, error } = useLevels();
+
+  if (isLoading) return <Text>Loading...</Text>;
+  if (error) return <Text>Error loading data</Text>;
+
+  return (
+    <View>
+      <Text>SubscriptionList</Text>
+      {data.map((level: { name: string }, index: number) => (
+        <RadioButton
+          key={index}
+          label={level.name}
+          price={index + 1 + '00 kr./mo.'}
+          value={index}
+          selected={selectedValue === index}
+          onSelect={setSelectedValue}
+          disabled={false}
+          //   disabled={level.name === disabledOption}
+        />
+      ))}
+    </View>
+  );
+};
+
